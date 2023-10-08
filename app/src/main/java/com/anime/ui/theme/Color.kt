82aebea8ef7900d25beb1aaa7336e4ea4580a994
Arturo Kuang <17709843+ArturoKuang@users.noreply.github.com>
2023-10-08 01:58:56 -0700

    add Detekt, Kotlint, and Compose lint check (#1)

--- app/src/main/java/com/anime/ui/theme/Color.kt
+++ app/src/main/java/com/anime/ui/theme/Color.kt
@@ -1,3 +1,4 @@
+@file:Suppress("MagicNumber")
 package com.anime.ui.theme
 
 import androidx.compose.ui.graphics.Color
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,19 @@
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("org.jmailen.kotlinter") version "3.16.0"
+    id("io.gitlab.arturbosch.detekt").version("1.23.1")
+}
+
+tasks {
+    val detektAll by registering(io.gitlab.arturbosch.detekt.Detekt::class) {
+        parallel = true
+        setSource(files(projectDir))
+        include("**/*.kt")
+        include("**/*.kts")
+        exclude("**/resources/**")
+        exclude("**/build/**")
+        config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+        buildUponDefaultConfig = false
+    }
 }
